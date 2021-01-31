5037b17c365ea243cab3c01bc8f1b55bfc949ab4
serge160889 <serge_voit_2006@mail.ru>
2021-01-31 22:18:26 +0300

    finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	Rectangle s1=new Rectangle(30);
 	Rectangle s2=new Rectangle(10,12);
-	Rectangle s3=new Rectangle(30);
-	double s1=s2.calculateArea();
-        System.out.println(s1);
-        double s4=s3.calculateArea();
-        System.out.println(s4);
+	Rectangle s3=new Rectangle(20,30);
+	double s4=s1.calculateArea();
+        System.out.println("площадь квадрата="+s4+"мм2");
+        double s5=s2.calculateArea();
+        System.out.println("площадь прямоугольника 1="+s5+"мм2");
+        double s6=s3.calculateArea();
+        System.out.println("площадь прямоугольника 2="+s6+"мм2");
+        s1.printArea();
         s2.printArea();
         s3.printArea();
+        s1.printRectangleKind();
+        s2.printRectangleKind();
+        s3.printRectangleKind();
+        boolean f1=s1.isTheSameRectangle(s2);
+        boolean f2=s2.isTheSameRectangle(s3);
+        boolean f3=s3.isTheSameRectangle(s1);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+
 
 
     }
--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -4,23 +4,47 @@ public class Rectangle {
     double x;
     double y;
 
-    public Rectangle(double x, double y){
-        this.x=x;
-        this.y=y;
+    public Rectangle(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
-    public Rectangle(double a){
-        this.x=a;
-        this.y=a;
+
+    public Rectangle(double a) {
+        this.x = a;
+        this.y = a;
 
     }
 
-     double calculateArea(){
+    double calculateArea() {
         double s;
-        s=x*y;
-      return s;
+        s = x * y;
+        return s;
     }
-    void printArea(){
-        System.out.println(calculateArea());
+
+    void printArea() {
+        System.out.println("площадь="+calculateArea()+"мм2");
 
     }
-}
+
+    void printRectangleKind() {
+        if (x != y) {
+            System.out.println("Это прямоугольник");
+        } else if (x == y) {
+            System.out.println("Это квадрат");
+        } else if (x == 0 | y == 0) {
+            System.out.println("Нет фигуры");
+
+        }
+    }
+    boolean isTheSameRectangle(Rectangle w){
+        if(this.x==w.x&&this.y==w.y)
+            return true;
+            else  return false;
+    }
+
+
+
+    }
+
+
+
